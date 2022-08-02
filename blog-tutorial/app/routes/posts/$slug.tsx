bad9bf5d03d87b611d91571250bee8c428f7087e
Gaku Yasui <gaku.yasui@gmail.com>
2022-08-02 01:22:19 +0000

    In the middle of jokes app tutorial

--- blog-tutorial/app/routes/posts/$slug.tsx
+++ blog-tutorial/app/routes/posts/$slug.tsx
@@ -10,25 +10,25 @@ import { getPost } from "~/models/post.server";
 type LoaderData = { post: Post; html: string };
 
 export const loader: LoaderFunction = async ({
-	params,
+  params,
 }) => {
-	invariant(params.slug, `params.slug is required`);
+  invariant(params.slug, `params.slug is required`);
 
-	const post = await getPost(params.slug);
-	invariant(post, `Post not found: ${params.slug}`);
+  const post = await getPost(params.slug);
+  invariant(post, `Post not found: ${params.slug}`);
 
-	const html = marked(post.markdown);
-	return json<LoaderData>({ post, html });
+  const html = marked(post.markdown);
+  return json<LoaderData>({ post, html });
 };
 
 export default function PostSlug() {
-	const { post, html } = useLoaderData() as LoaderData;
-	return (
-		<main className="mx-auto max-w-4xl">
-			<h1 className="my-6 border-b-2 text-center text-3xl">
-				{post.title}
-			</h1>
-			<div dangerouslySetInnerHTML={{ __html: html }} />
-		</main>
-	);
+  const { post, html } = useLoaderData() as LoaderData;
+  return (
+    <main className="mx-auto max-w-4xl">
+      <h1 className="my-6 border-b-2 text-center text-3xl">
+        {post.title}
+      </h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </main>
+  );
 }
